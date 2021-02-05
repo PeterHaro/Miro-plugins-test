fea1caae300d23057e331330645a50462f243f71
Peter Haro <peter.haro@sintef.no>
2021-02-05 11:41:39 +0100

    Experimenting with html

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ miro.onReady(() => {
                 onClick: function () {
                     miro.board.ui.openLeftSidebar('sidebar.html')
                 },
-                bottomBar: {
+                exportMenu: {
                     title: "Data exporter",
                     svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
                     onClick: function() {
